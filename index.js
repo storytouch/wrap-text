825574bf5e13f8ed9809b4735c2110f1d63b8406
Guilherme Goncalves <inacio.guilherme@gmail.com>
2020-07-07 17:09:01 -0300

    [refactor] Rewrite Chrome algorithm into a class

--- index.js
+++ index.js
@@ -1,93 +1,29 @@
-const DEFAULT_OPTIONS = {
-  browser: 'chrome',
-  richOutput: false,
-};
+const ChromeTextWrapper = require('./lib/chromeTextWrapper');
 
 const wrapText = (text, width, options = {}) => {
-  options = {
-    ...DEFAULT_OPTIONS,
-    ...options,
+  const defaultOptions = {
+    browser: 'chrome',
+    richOutput: false,
   };
 
-  // TODO implement the algorithms of other browsers
-  if (options.browser !== 'chrome') {
-    throw new Error('Browser not compatible yet');
-  }
+  options = {
+    ...defaultOptions,
+    ...options,
+  };
 
   // edge case
   if (width < 1) width = 1;
 
-  const outputLines = [];
-  let offset = 0;
-  let outputLine = '';
-
-  // split a word every $size characters.
-  const chunkWord = (str, size) => str.match(new RegExp(`.{1,${size}}`, 'g'));
-
-  // adds a new output line
-  const appendNewLine = (lineText) => {
-    outputLines.push({
-      line: lineText,
-      offset: offset - lineText.length,
-      length: lineText.length,
-    });
-    outputLine = '';
-  };
-
-  // adds a word into the last output line
-  const appendWord = (word) => {
-    // if a word is larger than the specified width
-    if (word.length > width) {
-      const subWords = chunkWord(word, width);
-      subWords.forEach((subWord) => appendWord(subWord));
-    } else {
-      if (word.length + outputLine.length > width) {
-        appendNewLine(outputLine);
-      }
-
-      outputLine += word;
-      offset += word.length;
-    }
-  };
-
-  const appendWhitespace = () => {
-    outputLine += ' ';
-    offset += 1;
-  };
-
-  // preserves existing line breaks
-  const inputLines = text.split('\n');
-  inputLines.forEach((inputLine, inputLineIndex) => {
-    if (inputLine.length === 0) {
-      appendNewLine('');
-    } else {
-      const words = inputLine.split(' ');
-      words.forEach((word, wordIndex) => {
-        const isLastWord = wordIndex === words.length - 1;
-
-        appendWord(word);
-
-        if (isLastWord) {
-          // if the word is the last one
-          appendNewLine(outputLine);
-        } else {
-          appendWhitespace();
-        }
-      });
-    }
-
-    // mark existing line breaks
-    offset += 1;
-    if (inputLineIndex < inputLines.length - 1) { // skip the end of text
-      outputLines[outputLines.length - 1].originalBreak = true;
-    }
-  });
-
-  if (!options.richOutput) {
-    return outputLines.map((entry) => entry.line);
+  let textWrapperAlgorithm;
+  switch (options.browser) {
+    case 'chrome':
+      textWrapperAlgorithm = new ChromeTextWrapper(options);
+      break;
+    default:
+      throw new Error('Browser not compatible yet');
   }
 
-  return outputLines;
+  return textWrapperAlgorithm.perform(text, width);
 };
 
 module.exports = wrapText;
